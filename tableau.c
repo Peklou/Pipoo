85e5589896023a59d2e5e8e0092f1125dd6d6efd
diego peribanez <peribanez.diego@gmail.com>
2021-11-16 20:10:44 +0100

    1.2 1.3 1.4 en cours de test

--- tableau.c
+++ tableau.c
@@ -1,17 +1,35 @@
 #include "tableau.h"
 #include <stdio.h>
+#include <stdlib.h>
 
-void* creertableau(int taille, int tailleMemoire) {
-	TABLEAU t;
+T creertableau(int taille, int tailleMemoire) {
+	T t;
 	t.data		= Malloc(taille * tailleMemoire);
 	t.taille	= taille;
 	t.tmemoire	= tailleMemoire;
 	return t;
 }
-void  afficher(TABLEAU t) {
+void  afficher(T t,int (*p)(void *)) {
 	void* ptr = t.data;
-	for (int i = 0; i < t.taille; i++;) {
+	for (int i = 0; i < t.taille; i++) {
 		ptr += t.tmemoire;
-		printf("%d",*(int *)ptr)
+		(*p)(ptr));
 	}
+}
+ 
+void  alleatoire(T t,void (*p)(void *)) {
+	void* ptr = t.data;
+	for (int i = 0; i < t.taille; i++) {
+		ptr += t.tmemoire;
+		(*p)(ptr) ;
+	}
+}
+
+
+void detruire_tout(TABLEAU t){
+    free(t.data);
+}
+void trier(TABLEAU T,int (*compare)){
+qsort(t.data,t.taille,t.tmemoire,&compare);
+
 }
